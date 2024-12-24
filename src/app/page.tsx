90f9f26c2ac646554604fbd41950fecf82b584df
amanyall <amanpandey5961@gmail.com>
2024-12-24 17:31:03 +0530

    deployment issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import Instructors from "@/components/Instructors";
 import MusicSchoolTestimonial from "@/components/TestimonialCards";
-import UpcomingWebinars from "@/components/ui/UpcomingWebinars";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import Image from "next/image";
 
